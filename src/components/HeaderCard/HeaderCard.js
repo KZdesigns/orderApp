5b499aa5ebc6c435083459c5783d9ff0154b5ed4
Kyle Zimmerman <kzdesignsios@gmail.com>
2022-07-17 16:29:21 -0700

    minor styling adjustments to Jumbotron and HeaderCard

--- src/components/HeaderCard/HeaderCard.js
+++ src/components/HeaderCard/HeaderCard.js
@@ -4,12 +4,12 @@ import classes from "./HeaderCard.module.css";
 const HeaderCard = () => {
   return (
     <div className={classes.container}>
-      <h1 style={{ margin: 0 }}>Delicious Food, Delivered To You</h1>
+      <h1 style={{ margin: "0 0 10px 0" }}>Delicious Food, Delivered To You</h1>
       <p style={{ margin: 0 }}>
         Choose your favorite meal from our broad selection of available meals
         and enjoy a delicious lunch or dinner at home.
       </p>
-      <p style={{ margin: 0 }}>
+      <p style={{ margin: "0 0 10px 0" }}>
         All our meals are cooked with high-quality ingredients, just-in-time and
         of course by experienced chefs!
       </p>
